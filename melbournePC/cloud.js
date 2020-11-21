36fe308d2cf96fcf04fbbe5a0edca47747409c0b
masonproco <43191303+masonproco@users.noreply.github.com>
2020-11-21 11:25:48 -0500

    Commit all single files
    Commit all single files

---
+++ melbournePC/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 353634995,
+    "boundingBox": {
+        "lx": 314499.99023717906,
+        "ly": 5808499.990054349,
+        "lz": -20.009874279782303,
+        "ux": 323500.0100574436,
+        "uy": 5817500.009874614,
+        "uz": 8980.009945984759
+    },
+    "tightBoundingBox": {
+        "lx": 314499.99596372959,
+        "ly": 5808499.995992534,
+        "lz": -20.00395087163839,
+        "ux": 323500.00305400459,
+        "uy": 5817500.003574752,
+        "uz": 302.02469177609938
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 77.94245910644531,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
